b19ba9761e8f0077379df4c01c70129279113ab6
ewoudtm <ewoud@ewoudtm.nl>
2017-04-13 20:49:14 +0200

    added roll-die.js

---
+++ src/actions/games/roll-die.js
@@ -0,0 +1,15 @@
+
+import { CALL_API, PATCH} from '../../middleware/api'
+
+export default (gameId) => {
+  return {
+    [CALL_API]: {
+      service: 'games',
+      method: PATCH,
+      type: 'GAME_UPDATED',
+      authenticate: true,
+      params: { rollDie: true },
+      id: gameId,
+    }
+  }
+}
